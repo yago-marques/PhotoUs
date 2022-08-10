4ca2b2a669676e10235438cc436aaaf653ef7a64
Yago Marques <yagovictormarques@gmail.com>
2022-08-10 11:55:55 -0300

    config gitignore

--- Pods/Target Support Files/Pods-PhotoUs-PhotoUsUITests/Pods-PhotoUs-PhotoUsUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PhotoUs_PhotoUsUITests : NSObject
-@end
-@implementation PodsDummy_Pods_PhotoUs_PhotoUsUITests
-@end
--- Pods/Target Support Files/Pods-PhotoUsTests/Pods-PhotoUsTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PhotoUsTests : NSObject
-@end
-@implementation PodsDummy_Pods_PhotoUsTests
-@end
